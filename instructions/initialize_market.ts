596cc1b864f8683777a46fade701975c1274e0f6
Will Beddow <will.beddow@toptal.com>
2021-12-14 07:58:30 -0700

    Add context to initialize market

--- instructions/initialize_market.ts
+++ instructions/initialize_market.ts
@@ -1,11 +1,13 @@
 import * as anchor from "@project-serum/anchor";
 import {PublicKey} from "@solana/web3.js";
+import Context from "../types/context";
 
 /**
  * Create a new serum market, with the specified authority
  *
+ * @param context Program context
  * @param authority
  */
-export default function initializeMarket(authority: PublicKey) {
+export default function initializeMarket(context: Context, authority: PublicKey) {
     let coinVaultWallet = anchor.web3.Keypair.generate();
 }
