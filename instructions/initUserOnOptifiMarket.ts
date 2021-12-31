b09e88552a72cea3383ca25b249b1370fb34a46f
Will Beddow <will.beddow@toptal.com>
2021-12-31 10:14:43 -0500

    Fix init user

--- instructions/initUserOnOptifiMarket.ts
+++ instructions/initUserOnOptifiMarket.ts
@@ -1,49 +1,68 @@
 import * as anchor from "@project-serum/anchor";
-import { PublicKey } from "@solana/web3.js";
+import {PublicKey} from "@solana/web3.js";
 import Context from "../types/context";
 import InstructionResult from "../types/instructionResult";
-import {findOptifiExchange, findPDA, findUserAccount, getDexOpenOrders, userAccountExists} from "../utils/accounts";
-import { formatExplorerAddress, SolanaEntityType } from "../utils/debug";
+import {findOptifiExchange, findUserAccount, getDexOpenOrders, userAccountExists} from "../utils/accounts";
+import {formatExplorerAddress, SolanaEntityType} from "../utils/debug";
+import {SERUM_DEX_PROGRAM_ID} from "../constants";
+import {OptifiMarket} from "../types/optifi-exchange-types";
+import {findSerumAuthorityPDA} from "../utils/pda";
+import {signAndSendTransaction, TransactionResultType} from "../utils/transactions";
 
 /**
- * Initialize a new open orders account for user to place order on the optifi market
+ * Initialize a new open orders account for user to place order on the optifi marketAddress
  * @param context
+ * @param marketAddress
  */
- export default async function initUserOnOptifiMarket(context: Context) : Promise<InstructionResult<string>> {
+ export default function initUserOnOptifiMarket(context: Context,
+                                                marketAddress: PublicKey,
+                                                ) : Promise<InstructionResult<string>> {
+     let serumId = new PublicKey(SERUM_DEX_PROGRAM_ID);
     return new Promise( (resolve, reject) => {
             userAccountExists(context).then(([exists, userAccount]) => {
                 if(!exists || !userAccount) reject({
                     successful: false,
                     error: "User account does not exist"
                 } as InstructionResult<any>)
-
-                findPDA(context).then(([pda, _bump]) => {
-                    findOptifiExchange(context).then(([exchangeAddress, bump]) => {
-                        getDexOpenOrders(context).then(([dexOpenOrders, bump2]) => {
-                            context.program.rpc.initUserOnOptifiMarket(bump2, {
-                                accounts: {
-                                  optifiExchange: exchangeAddress,
-                                  dexOpenOrders: dexOpenOrders,
-                                  optifiMarket: new PublicKey("9pgtcXRUfs9QbsZXBTSCtuMdZGkDabPRR6oFhgBkGXTM"),
-                                  serumMarket: context.user.publicKey,
-                                  serumDexProgramId: new PublicKey("DESVgJVGajEgKGXhb6XmqDHGz3VjdgP7rEVESBgxmroY"),
-                                  payer: context.user.publicKey,
-                                  pda: pda,
-                                  systemProgram: anchor.web3.SystemProgram.programId,
-                                  rent: anchor.web3.SYSVAR_RENT_PUBKEY,
-                                },
-                                // signers: [],
-                                instructions: [],
-                              }).then((tx) => {
-                                let txUrl = formatExplorerAddress(context, tx, SolanaEntityType.Transaction);
-                                console.log("Successfully initialized User on Optifi Market, ", txUrl);
-                                resolve({
-                                    successful: true,
-                                    data: txUrl,
+                context.program.account.market.fetch(marketAddress).then((marketRes) => {
+                    let optifiMarket = marketRes as OptifiMarket;
+                    findSerumAuthorityPDA(context).then(([serumAuthority, _]) => {
+                        findOptifiExchange(context).then(([exchangeAddress, _]) => {
+                            findUserAccount(context).then(([userAccountAddress, _]) => {
+                                getDexOpenOrders(context, marketAddress, userAccountAddress).then(([dexOpenOrders, bump]) => {
+                                    let initUserTx = context.program.transaction.initUserOnOptifiMarket(bump, {
+                                        accounts: {
+                                            optifiExchange: exchangeAddress,
+                                            user: context.provider.wallet.publicKey,
+                                            serumMarketAuthority: serumAuthority,
+                                            userAccount: userAccountAddress,
+                                            serumOpenOrders: dexOpenOrders,
+                                            optifiMarket: marketAddress,
+                                            serumMarket: optifiMarket.serumMarket,
+                                            serumDexProgramId: serumId,
+                                            payer: context.provider.wallet.publicKey,
+                                            systemProgram: anchor.web3.SystemProgram.programId,
+                                            rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+                                        },
+                                        instructions: [],
+                                    })
+                                    signAndSendTransaction(context, initUserTx).then((tx) => {
+                                        if (tx.resultType === TransactionResultType.Successful) {
+                                            let txUrl = formatExplorerAddress(context, tx.txId as string, SolanaEntityType.Transaction);
+                                            console.log("Successfully initialized user on Optifi Market, ", txUrl);
+                                            resolve({
+                                                successful: true,
+                                                data: txUrl,
+                                            })
+                                        } else {
+                                            console.error(tx);
+                                            reject(tx);
+                                        }
+                                    }).catch((err) => {
+                                        console.error("Got error trying to initialize User on Optifi Market", err);
+                                        reject(err);
+                                    })
                                 })
-                            }).catch((err) => {
-                                console.error("Got error trying to initialize User on Optifi Market", err);
-                                reject(err);
                             })
                         })
                     })
--- instructions/recordPnl.ts
+++ instructions/recordPnl.ts
@@ -1,8 +1,8 @@
 import Context from "../types/context";
 import InstructionResult from "../types/instructionResult";
 import {PublicKey} from "@solana/web3.js";
-import {findExchangeAccount, getDexOpenOrders} from "../utils/accounts";
-import {OptifiMarket} from "../types/optifi-exchange-types";
+import {findExchangeAccount, findUserAccount, getDexOpenOrders} from "../utils/accounts";
+import {OptifiMarket, UserAccount} from "../types/optifi-exchange-types";
 import {deriveVaultNonce, getSerumMarket} from "../utils/market";
 import {SERUM_DEX_PROGRAM_ID} from "../constants";
 
@@ -12,36 +12,46 @@ export default function recordPnl(context: Context,
                                   market: PublicKey,
                                   instrument: PublicKey): Promise<InstructionResult<string>> {
     return new Promise((resolve, reject) => {
-        findExchangeAccount(context).then(([exchangeAddress, _]) => {
-            context.program.account.optifiMarket.fetch(market).then((res) => {
-                let optifiMarket = res as OptifiMarket;
-                let serumMarketAddress = optifiMarket.serumMarket;
-                deriveVaultNonce(serumMarketAddress, new PublicKey(SERUM_DEX_PROGRAM_ID))
-                    .then(([vaultAddress, nonce]) => {
-
-                        getSerumMarket(context, serumMarketAddress).then((serumMarket) => {
-                            getDexOpenOrders(context, serumMarketAddress, userToSettle).then(([openOrdersAccount, _]) => {
-                                let settlementTx = context.program.transaction.recordPnlForOneUser({
-                                    optifiExchange: exchangeAddress,
-                                    userAccount: userToSettle,
-                                    optifiMarket: market,
-                                    serumMarket: serumMarketAddress,
-                                    userSerumOpenOrders: openOrdersAccount,
-                                    instrument: instrument,
-                                    bids: serumMarket.bidsAddress,
-                                    asks: serumMarket.asksAddress,
-                                    eventQueue: serumMarket.decoded.eventQueue,
-                                    coinVault: serumMarket.decoded.baseVault,
-                                    pcVault: serumMarket.decoded.quoteVault,
-                                    vaultSigner: vaultAddress,
-                                    userMarginAccount:
+        findUserAccount(context).then(([userAccountAddress, _]) => {
+            context.program.account.userAccount.fetch(userAccountAddress).then((userAcctRaw) => {
+                // @ts-ignore
+                let userAccount = userAcctRaw as UserAccount;
+                findExchangeAccount(context).then(([exchangeAddress, _]) => {
+                    context.program.account.optifiMarket.fetch(market).then((res) => {
+                        let optifiMarket = res as OptifiMarket;
+                        let serumMarketAddress = optifiMarket.serumMarket;
+                        deriveVaultNonce(serumMarketAddress, new PublicKey(SERUM_DEX_PROGRAM_ID))
+                            .then(([vaultAddress, nonce]) => {
+                                getSerumMarket(context, serumMarketAddress).then((serumMarket) => {
+                                    getDexOpenOrders(context, serumMarketAddress, userToSettle).then(([openOrdersAccount, _]) => {
+                                        let settlementTx = context.program.transaction.recordPnlForOneUser({
+                                            optifiExchange: exchangeAddress,
+                                            userAccount: userToSettle,
+                                            optifiMarket: market,
+                                            serumMarket: serumMarketAddress,
+                                            userSerumOpenOrders: openOrdersAccount,
+                                            instrument: instrument,
+                                            bids: serumMarket.bidsAddress,
+                                            asks: serumMarket.asksAddress,
+                                            eventQueue: serumMarket.decoded.eventQueue,
+                                            coinVault: serumMarket.decoded.baseVault,
+                                            pcVault: serumMarket.decoded.quoteVault,
+                                            vaultSigner: vaultAddress,
+                                            userMarginAccount: userAccount.userMarginAccountUsdc,
+                                            instrumentLongSplTokenMint: optifiMarket.instrumentLongSplToken,
+                                            instrumentShortSplTokenMint: optifiMarket.instrumentShortSplToken,
+                                            userInstrumentLongTokenVault: userAccount
+                                            userInstrumentShortTokenVault: userAccount
+                                        })
+                                    })
                                 })
                             })
-                        })
+                    }).catch((err) => {
+                        console.error(err);
+                        reject(err);
+                    })
                 })
-            }).catch((err) => {
-                console.error(err);
-                reject(err);
+
             })
         })
     })
