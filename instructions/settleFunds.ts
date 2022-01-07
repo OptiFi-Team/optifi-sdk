778f199fd0ad342fe2fca100a062752c8b04541f
Will Beddow <will.beddow@toptal.com>
2022-01-07 13:59:07 -0600

    Small correction to settle funds

--- instructions/settleFunds.ts
+++ instructions/settleFunds.ts
@@ -10,39 +10,36 @@ import {TOKEN_PROGRAM_ID} from "@solana/spl-token";
 import {signAndSendTransaction, TransactionResultType} from "../utils/transactions";
 
 export default function settleFunds(context: Context,
-                                    marketAddress: PublicKey,
-                                    instrumentAddress: PublicKey): Promise<InstructionResult<TransactionSignature>> {
+                                    userToSettle: PublicKey): Promise<InstructionResult<TransactionSignature>> {
     return new Promise((resolve, reject) => {
         findExchangeAccount(context).then(([exchangeAddress, _]) => {
-            findUserAccount(context).then(([userAccountAddress, _]) => {
-                context.program.account.userAccount.fetch(userAccountAddress).then((userAccountRes) => {
-                    // @ts-ignore
-                    let userAccount = userAccountRes as UserAccount;
-                    findOptifiUSDCPoolAuthPDA(context).then(([centralUSDCPoolAuth, _]) => {
-                        context.program.account.exchange.fetch(exchangeAddress).then((exchangeRes) => {
-                            let exchange = exchangeRes as Exchange;
-                            let settleTx = context.program.transaction.settleFundForOneUser({
-                                accounts: {
-                                    optifiExchange: exchangeAddress,
-                                    userAccount: userAccountAddress,
-                                    userMarginAccountUsdc: userAccount.userMarginAccountUsdc,
-                                    centralUsdcPool: exchange.usdcCentralPool,
-                                    centralUsdcPoolAuth: centralUSDCPoolAuth,
-                                    usdcMint: new PublicKey(USDC_TOKEN_MINT[context.endpoint]),
-                                    tokenProgram: TOKEN_PROGRAM_ID
-                                }
-                            });
-                            signAndSendTransaction(context, settleTx).then((settleRes) => {
-                                if (settleRes.resultType === TransactionResultType.Successful) {
-                                    resolve({
-                                        successful: true,
-                                        data: settleRes.txId as TransactionSignature
-                                    })
-                                } else {
-                                    console.error(settleRes);
-                                    reject(settleRes);
-                                }
-                            }).catch((err) => reject(err))
+            context.program.account.userAccount.fetch(userToSettle).then((userAccountRes) => {
+                // @ts-ignore
+                let userAccount = userAccountRes as UserAccount;
+                findOptifiUSDCPoolAuthPDA(context).then(([centralUSDCPoolAuth, _]) => {
+                    context.program.account.exchange.fetch(exchangeAddress).then((exchangeRes) => {
+                        let exchange = exchangeRes as Exchange;
+                        let settleTx = context.program.transaction.settleFundForOneUser({
+                            accounts: {
+                                optifiExchange: exchangeAddress,
+                                userAccount: userToSettle,
+                                userMarginAccountUsdc: userAccount.userMarginAccountUsdc,
+                                centralUsdcPool: exchange.usdcCentralPool,
+                                centralUsdcPoolAuth: centralUSDCPoolAuth,
+                                usdcMint: new PublicKey(USDC_TOKEN_MINT[context.endpoint]),
+                                tokenProgram: TOKEN_PROGRAM_ID
+                            }
+                        });
+                        signAndSendTransaction(context, settleTx).then((settleRes) => {
+                            if (settleRes.resultType === TransactionResultType.Successful) {
+                                resolve({
+                                    successful: true,
+                                    data: settleRes.txId as TransactionSignature
+                                })
+                            } else {
+                                console.error(settleRes);
+                                reject(settleRes);
+                            }
                         }).catch((err) => reject(err))
                     }).catch((err) => reject(err))
                 }).catch((err) => reject(err))
