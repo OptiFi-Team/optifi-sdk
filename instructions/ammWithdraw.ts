263ea06001daacfebe6b698ad798d1cddc2ea5bb
Will Beddow <will.beddow@toptal.com>
2022-01-09 13:38:16 -0600

    Version bump for publish, and fix amm withdraw

--- instructions/ammWithdraw.ts
+++ instructions/ammWithdraw.ts
@@ -7,6 +7,7 @@ import {Amm} from "../types/optifi-exchange-types";
 import {TOKEN_PROGRAM_ID} from "@solana/spl-token";
 import {signAndSendTransaction, TransactionResultType} from "../utils/transactions";
 import {findAssociatedTokenAccount} from "../utils/token";
+import {getAmmLiquidityAuthPDA} from "../utils/pda";
 
 export default function ammWithdraw(context: Context,
                                     ammAddress: PublicKey,
@@ -14,43 +15,54 @@ export default function ammWithdraw(context: Context,
     return new Promise((resolve, reject) => {
         findExchangeAccount(context).then(([exchangeAddress, _]) => {
             findUserAccount(context).then(([userAccountAddress, _]) => {
-                context.program.account.amm.fetch(ammAddress).then((ammRes) => {
-                    // @ts-ignore
-                    let amm = ammRes as Amm;
-                    findAssociatedTokenAccount(context, amm.quoteTokenMint).then(([userQuoteTokenVault, _]) => {
-                        findAssociatedTokenAccount(context, amm.lpTokenMint).then(([userLpTokenVault, _]) => {
-                            let ammWithdrawTx = context.program.transaction.ammWithdraw(
-                                new anchor.BN(amount),
-                                {
-                                    accounts: {
-                                        optifiExchange: exchangeAddress,
-                                        amm: ammAddress,
-                                        userAccount: userAccountAddress,
-                                        ammQuoteTokenMint: amm.quoteTokenMint,
-                                        ammQuoteTokenVault: amm.quoteTokenVault,
-                                        userQuoteTokenVault: userQuoteTokenVault,
-                                        lpTokenMint: amm.lpTokenMint,
-                                        userLpTokenVault: userLpTokenVault,
-                                        user: context.provider.wallet.publicKey,
-                                        tokenProgram: TOKEN_PROGRAM_ID
+                getAmmLiquidityAuthPDA(context).then(([liquidityAuthPDA, _]) => {
+                    context.program.account.amm.fetch(ammAddress).then((ammRes) => {
+                        // @ts-ignore
+                        let amm = ammRes as Amm;
+                        findAssociatedTokenAccount(context, amm.quoteTokenMint).then(([userQuoteTokenVault, _]) => {
+                            findAssociatedTokenAccount(context, amm.lpTokenMint).then(([userLpTokenVault, _]) => {
+                                let ammWithdrawTx = context.program.transaction.ammWithdraw(
+                                    new anchor.BN(amount),
+                                    {
+                                        accounts: {
+                                            optifiExchange: exchangeAddress,
+                                            amm: ammAddress,
+                                            userAccount: userAccountAddress,
+                                            ammQuoteTokenVault: amm.quoteTokenVault,
+                                            userQuoteTokenVault: userQuoteTokenVault,
+                                            lpTokenMint: amm.lpTokenMint,
+                                            userLpTokenVault: userLpTokenVault,
+                                            user: context.provider.wallet.publicKey,
+                                            tokenProgram: TOKEN_PROGRAM_ID,
+                                            ammLiquidityAuth: liquidityAuthPDA
+                                        }
                                     }
-                                }
-                            );
-                            signAndSendTransaction(context, ammWithdrawTx).then((ammWithdrawRes) => {
-                                if (ammWithdrawRes.resultType === TransactionResultType.Successful) {
-                                    resolve({
-                                        successful: true,
-                                        data: ammWithdrawRes.txId as TransactionSignature
-                                    })
-                                } else {
-                                    console.error(ammWithdrawRes);
-                                    reject(ammWithdrawRes);
-                                }
+                                );
+                                signAndSendTransaction(context, ammWithdrawTx).then((ammWithdrawRes) => {
+                                    if (ammWithdrawRes.resultType === TransactionResultType.Successful) {
+                                        resolve({
+                                            successful: true,
+                                            data: ammWithdrawRes.txId as TransactionSignature
+                                        })
+                                    } else {
+                                        console.error(ammWithdrawRes);
+                                        reject(ammWithdrawRes);
+                                    }
+                                }).catch((err) => {
+                                    console.error(err);
+                                    reject(err);
+                                })
+                            }).catch((err) => {
+                                console.error(err);
+                                reject(err);
                             })
+                        }).catch((err) => {
+                            console.error(err);
+                            reject(err)
                         })
-                    })
-                })
-            })
-        })
+                    }).catch((err) => reject(err))
+                }).catch((err) => reject(err))
+            }).catch((err) => reject(err))
+        }).catch((err) => reject(err))
     })
 }
