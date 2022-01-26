0cc6fb3b34cc00f9f4d211127bddbc0fcf9c095f
WayneAl <wayne.k@alphabit.software>
2022-01-26 15:35:05 +0800

    Update placeOrder.ts

--- scripts/placeOrder.ts
+++ scripts/placeOrder.ts
@@ -8,31 +8,36 @@ import {settleSerumFundsIfAnyUnsettled, watchSettleSerumFunds} from "../utils/se
 
 let market = new PublicKey("BzpWij8iXh3t6VFaJ5NWLi6yCNkT24gVTax141LNLGnL");
 let limit = 1;
-let maxCoinQty = 1;
-let maxPcQty = 1;
+let maxCoinQty = 1; // should be integer
 
-let side = OrderSide.Ask;
+let side = OrderSide.Bid;
 
 initializeContext().then((context) => {
-    formOrderContext(context, market, OrderSide.Ask).then((orderContext) => {
-        console.log("Depositing...");
+    formOrderContext(context, market, side).then((orderContext) => {
         console.log("Serum market is ", formatExplorerAddress(context, orderContext.serumMarket.toString(), SolanaEntityType.Account));
         console.log("Open orders account is ", formatExplorerAddress(context, orderContext.openOrders.toString(), SolanaEntityType.Account))
-        placeOrder(context, market, OrderSide.Ask, limit, maxCoinQty, maxPcQty).then(async (res) => {
-            console.log("Placed order ", res);
-            if (res.successful) {
-                console.log(formatExplorerAddress(context, res.data as TransactionSignature, SolanaEntityType.Transaction));
-                // @ts-ignore
-                if (side === OrderSide.Bid) {
-                    await watchSettleSerumFunds(context, market).then((res) => {
-                        console.log("Got res!");
-                    }).catch((err) => {
-                        console.error(err);
-                    })
+        context.connection.getTokenAccountBalance(orderContext.userMarginAccount).then(tokenAmount => {
+            console.log("userMarginAccount: ", orderContext.userMarginAccount.toString());
+            console.log("balance: ", tokenAmount.value.uiAmount);
+            let maxPcQty = limit * (10 ** tokenAmount.value.decimals) * maxCoinQty;
+            placeOrder(context, market, side, limit * (10 ** tokenAmount.value.decimals), maxCoinQty, maxPcQty).then(async (res) => {
+                console.log("Placed order ", res);
+                if (res.successful) {
+                    console.log(formatExplorerAddress(context, res.data as TransactionSignature, SolanaEntityType.Transaction));
+                    // @ts-ignore
+                    if (side === OrderSide.Bid) {
+                        await watchSettleSerumFunds(context, market).then((res) => {
+                            console.log("Got res!");
+                        }).catch((err) => {
+                            console.error(err);
+                        })
+                    }
+                } else {
+                    console.error(res);
                 }
-            } else {
-                console.error(res);
-            }
+            }).catch((err) => {
+                console.error(err)
+            })
         }).catch((err) => {
             console.error(err)
         })
