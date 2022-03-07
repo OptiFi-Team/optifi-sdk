9ca3b7e926bd2b8132f16d7fff74c5f2180489e5
Wayne Kuo <wayne.k@alphabit.software>
2022-03-07 13:03:34 +0800

    Update getUserMargin.ts

--- scripts/getUserMargin.ts
+++ scripts/getUserMargin.ts
@@ -3,9 +3,12 @@ import { getAmountToReserve, getUserBalance } from "../utils/user";
 
 initializeContext().then(async (context) => {
     let amount_to_reserve = await getAmountToReserve(context);
-    let user_balance = await getUserBalance(context);
+    let user_balance = await getUserBalance(context); // total balance
     let available_balance = user_balance - amount_to_reserve;
-    let liquidation = available_balance * 0.9;
+
+    let liquidation = amount_to_reserve * 0.9;
+    let liquidation_buffer = user_balance - liquidation;
+
 
 
     console.log("amount_to_reserve: ", amount_to_reserve);
