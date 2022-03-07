a6d374fb92f671d98e828626454f626d3700efe7
Wayne Kuo <wayne.k@alphabit.software>
2022-03-07 19:09:19 +0800

    Update liquidation

--- instructions/initLiquidation.ts
+++ instructions/initLiquidation.ts
@@ -1,28 +1,29 @@
 import Context from "../types/context";
-import {PublicKey, TransactionSignature} from "@solana/web3.js";
+import { PublicKey, TransactionSignature } from "@solana/web3.js";
 import InstructionResult from "../types/instructionResult";
-import {findLiquidationState} from "../utils/accounts";
-import {signAndSendTransaction, TransactionResultType} from "../utils/transactions";
+import { findExchangeAccount, findLiquidationState } from "../utils/accounts";
+import { signAndSendTransaction, TransactionResultType } from "../utils/transactions";
+import { UserAccount } from "../types/optifi-exchange-types";
 
 export default function initLiquidation(context: Context, userToLiquidate: PublicKey): Promise<InstructionResult<TransactionSignature>> {
     return new Promise((resolve, reject) => {
-        findLiquidationState(context, userToLiquidate).then(([liquidationStateAddress, _]) => {
-            let initLiquidateTx = context.program.transaction.initLiquidation({
-                accounts: {
-                    userAccount: userToLiquidate,
-                    liquidationState: liquidationStateAddress
-                }
-            });
-            signAndSendTransaction(context, initLiquidateTx).then((res) => {
-                if (res.resultType === TransactionResultType.Successful) {
-                    resolve({
-                        successful: true,
-                        data: res.txId as TransactionSignature
-                    })
-                } else {
-                    console.error(res);
-                    reject(res);
-                }
+        findExchangeAccount(context).then(([exchangeAddress, _]) => {
+            findLiquidationState(context, userToLiquidate).then(([liquidationStateAddress, _]) => {
+                context.program.account.userAccount.fetch(userToLiquidate).then((userAccount) => {
+                    context.program.rpc.initLiquidation({
+                        accounts: {
+                            optifiExchange: exchangeAddress,
+                            userAccount: userToLiquidate,
+                            userMarginAccountUsdc: userAccount.userMarginAccountUsdc,
+                            liquidationState: liquidationStateAddress
+                        }
+                    }).then((res) => {
+                        resolve({
+                            successful: true,
+                            data: res as TransactionSignature
+                        })
+                    }).catch((err) => reject(err))
+                }).catch((err) => reject(err))
             }).catch((err) => reject(err))
         }).catch((err) => reject(err))
     })
