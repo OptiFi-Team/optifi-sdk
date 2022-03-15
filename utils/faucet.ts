ef9a10bc8f5fbffaeb34df4d750e248038078bc9
Prince Gao <prince.gao@aloha.software>
2022-03-15 16:42:21 +0800

    fixed file import issue

--- utils/faucet.ts
+++ utils/faucet.ts
@@ -2,7 +2,7 @@ import { Transaction, PublicKey, Keypair } from "@solana/web3.js";
 import { createAssociatedTokenAccountInstruction, createTransferInstruction, getAssociatedTokenAddress, getOrCreateAssociatedTokenAccount, TOKEN_PROGRAM_ID } from "@solana/spl-token";
 import { SolanaEndpoint, USDC_DECIMALS, USDC_TOKEN_MINT } from "../constants"
 import Context from "../types/context";
-import testWallet from "../test_account.json";
+// import testWallet from "../test_account.json";
 
 import { initializeContext } from "../index";
 
@@ -17,7 +17,7 @@ export default async function airdropUSDC(context: Context) {
 
     let fromWallet = Keypair.fromSecretKey(
         new Uint8Array(
-            testWallet
+            wallet
         ))
     // console.log("fromWallet: ", fromWallet.publicKey.toString())
     let usdcMint = new PublicKey(USDC_TOKEN_MINT[SolanaEndpoint.Devnet])
