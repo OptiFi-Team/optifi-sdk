302ac0ca35a204dfd2684ff886e00df4a71fbf9c
Prince Gao <prince.gao@aloha.software>
2022-03-22 16:35:18 +0800

    updated amm  scripts

--- scripts/amm/initializeAMMs.ts
+++ scripts/amm/initializeAMMs.ts
@@ -9,7 +9,8 @@ import { USDC_DECIMALS } from "../../constants";
 async function initializeAMMOnSupportedAssets(context: Context) {
     let i = 1;
     let duration = Duration.Monthly; // should be the same as created instruments
-    let contractSize = 0.01 * (10 ** USDC_DECIMALS); // TBD
+    // let contractSize = 0.01 * (10 ** USDC_DECIMALS); // TBD
+    let contractSize = 0.01 * (10 ** 4); // TBD
     for (let asset of SUPPORTED_ASSETS) {
         console.log("Initializing AMM for asset ", asset);
         try {
