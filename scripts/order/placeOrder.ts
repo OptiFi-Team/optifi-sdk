65a190f6264e1d64947ffe0bb4045a7effbcfb80
Wayne Kuo <wayne.k@alphabit.software>
2022-03-23 17:12:24 +0800

    Update idl

--- scripts/order/placeOrder.ts
+++ scripts/order/placeOrder.ts
@@ -6,10 +6,10 @@ import { formatExplorerAddress, SolanaEntityType } from "../../utils/debug";
 import { userAccountExists } from "../../utils/accounts";
 import { UserAccount } from "../../types/optifi-exchange-types";
 
-let market = new PublicKey("5P8ByngKaQJeaSpjgJmo9o2dSxBXzp9cU5xjkDALEqCR");
+let market = new PublicKey("GHtSNAhYsgPUcg4ZTPjp5g4ttq2cqaJBvt7YiHEVqbwb");
 let price = 1000;
 let size = 0.01; // should be integer
-let side = OrderSide.Bid;
+let side = OrderSide.Ask;
 
 initializeContext().then((context) => {
     userAccountExists(context).then(async ([_, res]) => {
