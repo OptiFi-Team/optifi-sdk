91fd2fcf00d328e0053bf82aa0578c7b89541739
Prince Gao <prince.gao@aloha.software>
2022-03-25 22:07:14 +0800

    optimised get user positions

--- scripts/logUserPositions.ts
+++ scripts/logUserPositions.ts
@@ -1,5 +1,5 @@
 import { initializeContext } from "../index";
-import { findOptifiMarkets, getPosition, isUserInitializedOnMarket } from "../utils/market";
+import { findOptifiMarkets, getPosition, getUserPositions, isUserInitializedOnMarket } from "../utils/market";
 import { findOrCreateAssociatedTokenAccount } from "../utils/token";
 import { findUserAccount } from "../utils/accounts";
 import { UserAccount } from "../types/optifi-exchange-types";
@@ -7,24 +7,28 @@ import UserPosition from "../types/user";
 
 initializeContext().then((context) => {
     findUserAccount(context).then(([userAccountAddress, _]) => {
-        findOptifiMarkets(context).then(async (markets) => {
-            let res = await context.program.account.userAccount.fetch(userAccountAddress);
-            // @ts-ignore
-            let userAccount = res as UserAccount;
-            let positions = userAccount.positions as UserPosition[];
-            let tradingMarkets = markets.filter(market => positions.map(e => e.toString()).includes(market[0].instrument.toString()));
-            console.log(positions);
-            Promise.all(
-                tradingMarkets.map(async (market) => {
-                    let [longAmount, shortAmount] = await getPosition(context,
-                        market[0],
-                        userAccountAddress,
-                    );
-                    console.log(`market: ${market[1]}\n`);
-                    console.log(`long tokens: ${longAmount}\n`);
-                    console.log(`short tokens: ${shortAmount}\n`);
-                })
-            )
-        }).catch((err) => console.log(err))
+        // findOptifiMarkets(context).then(async (markets) => {
+        //     let res = await context.program.account.userAccount.fetch(userAccountAddress);
+        //     // @ts-ignore
+        //     let userAccount = res as UserAccount;
+        //     let positions = userAccount.positions as UserPosition[];
+        //     let tradingMarkets = markets.filter(market => positions.map(e => e.toString()).includes(market[0].instrument.toString()));
+        //     console.log(positions);
+        //     Promise.all(
+        //         tradingMarkets.map(async (market) => {
+        //             let [longAmount, shortAmount] = await getPosition(context,
+        //                 market[0],
+        //                 userAccountAddress,
+        //             );
+        //             console.log(`market: ${market[1]}\n`);
+        //             console.log(`long tokens: ${longAmount}\n`);
+        //             console.log(`short tokens: ${shortAmount}\n`);
+        //         })
+        //     )
+        // }).catch((err) => console.log(err))
+
+        getUserPositions(context, userAccountAddress).then(res => {
+            console.log(res)
+        })
     })
 })
