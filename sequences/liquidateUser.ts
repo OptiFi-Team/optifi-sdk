1d6018426b18667ebe9e2e3d96c1848ca26a2af2
LeoOptifi <chun.w@alphabit.software>
2022-04-14 11:41:47 +0800

    sort liquidationMarkets by liquidationValues

--- sequences/liquidateUser.ts
+++ sequences/liquidateUser.ts
@@ -13,36 +13,74 @@ import liquidationSettleOrder from "../instructions/liquidation/liquidationSettl
 import { getSerumMarket } from "../utils/serum";
 import { findOptifiMarkets } from "../utils/market";
 import { sleep } from "../utils/generic";
+import { sortAndDeduplicateDiagnostics } from "typescript";
+import { resolve } from "path";
+import { rejects } from "assert";
+
+async function sortMarketsFromValues(liquidationMarkets: PublicKey[], liquidationValues: number[])
+    : Promise<[PublicKey[], number[]]> {
+    return new Promise((resolve, rejects) => {
+        try {
+            if (liquidationMarkets.length != liquidationValues.length) {
+                console.log("liquidationMarkets is not equal to liquidationValues");
+            }
+
+            for (let i = 0; i < liquidationValues.length; i++) {
+
+                let min = liquidationValues[i];
+                let marketsMinValues = liquidationMarkets[i];
+                let minIndex = i;
+
+                for (let j = i; j < liquidationValues.length; j++) {
+                    if (liquidationValues[j] < min) {
+                        min = liquidationValues[j];
+                        marketsMinValues = liquidationMarkets[j];
+
+                        minIndex = j;
+                    }
+                }
+
+                [liquidationValues[minIndex], liquidationValues[i]] = [liquidationValues[i], liquidationValues[minIndex]];
+                [liquidationMarkets[minIndex], liquidationMarkets[i]] = [liquidationMarkets[i], liquidationMarkets[minIndex]];
+            }
+            resolve([liquidationMarkets, liquidationValues]);
+        } catch (err) {
+            rejects(err)
+        }
+    })
+
+
+}
 
 export default async function liquidateUser(context: Context, userToLiquidate: PublicKey)
     : Promise<void> {
-    // // update margin requirement
-    // await marginCalculate(context, userToLiquidate);
+    // update margin requirement
+    await marginCalculate(context, userToLiquidate);
 
-    // // initLiquidation
-    // console.log("Start initialize liquidation...");
+    // initLiquidation
+    console.log("Start initialize liquidation...");
 
-    // await initLiquidation(context, userToLiquidate).then((res) => {
-    //     console.log("Got initLiquidation res", res);
-    // }).catch((err) => {
-    //     console.error(err);
-    // })
+    await initLiquidation(context, userToLiquidate).then((res) => {
+        console.log("Got initLiquidation res", res);
+    }).catch((err) => {
+        console.error(err);
+    })
 
-    // // registerLiquidationMarket
-    // console.log("Start register markets and cancel orders...");
-    // let res = await context.program.account.userAccount.fetch(userToLiquidate);
-    // let userAccount = res as unknown as UserAccount;
-    // let tradingMarkets = userAccount.tradingMarkets;
+    // registerLiquidationMarket
+    console.log("Start register markets and cancel orders...");
+    let res = await context.program.account.userAccount.fetch(userToLiquidate);
+    let userAccount = res as unknown as UserAccount;
+    let tradingMarkets = userAccount.tradingMarkets;
 
-    // for (let marketAddress of tradingMarkets) {
-    //     await registerLiquidationMarket(context, userToLiquidate, marketAddress).then((res) => {
-    //         console.log("Got registerLiquidationMarket res", res, "on market", marketAddress.toString());
-    //     }).catch((err) => {
-    //         console.error(err);
-    //     })
-    // }
-    // console.log("Wait 10 secs...");
-    // await sleep(10000);
+    for (let marketAddress of tradingMarkets) {
+        await registerLiquidationMarket(context, userToLiquidate, marketAddress).then((res) => {
+            console.log("Got registerLiquidationMarket res", res, "on market", marketAddress.toString());
+        }).catch((err) => {
+            console.error(err);
+        })
+    }
+    console.log("Wait 10 secs...");
+    await sleep(10000);
 
     let liquidate = async () => {
         let [liquidationStateAddress, _] = await findLiquidationState(context, userToLiquidate);
@@ -51,7 +89,11 @@ export default async function liquidateUser(context: Context, userToLiquidate: P
         let liquidationMarkets = liquidationState.markets;
         let liquidationValues = liquidationState.values.map(v => v.toNumber());
         // sort the liquidationMarkets by values
-
+        console.log("liquidationMarkets before: " + liquidationMarkets);
+        console.log("liquidationValues before: " + liquidationValues);
+        [liquidationMarkets, liquidationValues] = await sortMarketsFromValues(liquidationMarkets, liquidationValues);
+        console.log("liquidationMarkets after: " + liquidationMarkets);
+        console.log("liquidationValues after: " + liquidationValues);
 
         let marketsWithKeys = await findOptifiMarkets(context, liquidationMarkets);
         // the length of marketsWithKeys should not be zero
