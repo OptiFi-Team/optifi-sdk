99ad23c4c6a35d121460b18a6002606193ed89d2
Prince Gao <prince.gao@aloha.software>
2022-04-23 00:14:28 +0800

    updated add instrument to amm

--- scripts/amm/addInstrumentToAmm.ts
+++ scripts/amm/addInstrumentToAmm.ts
@@ -25,7 +25,7 @@ export async function addInstrumentsToAmm(context: Context, ammIndex: number) {
         let instrumentRawInfos = await context.program.account.chain.fetchMultiple(instrumentAddresses)
         let instrumentInfos = instrumentRawInfos as Chain[]
         instrumentInfos.forEach((instrument, i) => {
-            if (instrument.asset == ammInfo.asset) {
+            if (instrument.asset == ammInfo.asset && !ammInfo.tradingInstruments.map(e => e.toString()).includes(instrumentAddresses[i].toString())) {
                 optifiMarketsToAdd.push(optifiMarkets[i][1])
             }
         })
