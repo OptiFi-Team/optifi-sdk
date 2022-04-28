43fa1476f01fc69951c0d1e8e73fc30b76279736
Prince Gao <prince.gao@aloha.software>
2022-04-28 09:21:14 +0800

    1)rmoved amm cancel order; 2) updated mm register inx

--- instructions/registerMarketMaker.ts
+++ instructions/registerMarketMaker.ts
@@ -1,11 +1,11 @@
 import Context from "../types/context";
 import InstructionResult from "../types/instructionResult";
-import {PublicKey, SystemProgram, SYSVAR_CLOCK_PUBKEY, TransactionSignature} from "@solana/web3.js";
-import {findExchangeAccount, findMarketMakerAccount, findUserAccount} from "../utils/accounts";
-import {findOrCreateAssociatedTokenAccount} from "../utils/token";
-import {USDC_TOKEN_MINT} from "../constants";
-import {signAndSendTransaction, TransactionResultType} from "../utils/transactions";
-import {formatExplorerAddress, SolanaEntityType} from "../utils/debug";
+import { PublicKey, SystemProgram, SYSVAR_CLOCK_PUBKEY, SYSVAR_RENT_PUBKEY, TransactionSignature } from "@solana/web3.js";
+import { findExchangeAccount, findMarketMakerAccount, findUserAccount } from "../utils/accounts";
+import { findOrCreateAssociatedTokenAccount } from "../utils/token";
+import { USDC_TOKEN_MINT } from "../constants";
+import { signAndSendTransaction, TransactionResultType } from "../utils/transactions";
+import { formatExplorerAddress, SolanaEntityType } from "../utils/debug";
 
 export default function registerMarketMaker(context: Context): Promise<InstructionResult<TransactionSignature>> {
     return new Promise((resolve, reject) => {
@@ -13,19 +13,18 @@ export default function registerMarketMaker(context: Context): Promise<Instructi
             findUserAccount(context).then(([userAccountAddress, _]) => {
                 findMarketMakerAccount(context).then(([marketMakerAccount]) => {
                     findOrCreateAssociatedTokenAccount(context,
-                            new PublicKey(USDC_TOKEN_MINT[context.endpoint]),
+                        new PublicKey(USDC_TOKEN_MINT[context.endpoint]),
                         marketMakerAccount).then((liquidityPoolAccount) => {
                             let registerMarketMakerTx = context.program.transaction.registerMarketMaker({
                                 accounts: {
                                     optifiExchange: exchangeAddress,
                                     userAccount: userAccountAddress,
                                     marketMakerAccount: marketMakerAccount,
-                                    liquidityPool: liquidityPoolAccount,
-                                    owner: context.provider.wallet.publicKey,
+                                    user: context.provider.wallet.publicKey,
                                     systemProgram: SystemProgram.programId,
-                                    clock: SYSVAR_CLOCK_PUBKEY
+                                    rent: SYSVAR_RENT_PUBKEY
                                 }
-                        });
+                            });
                             signAndSendTransaction(context, registerMarketMakerTx).then((txRes) => {
                                 if (txRes.resultType === TransactionResultType.Successful) {
                                     console.log("Successfully registered market maker",
@@ -40,7 +39,7 @@ export default function registerMarketMaker(context: Context): Promise<Instructi
                                 console.error(err);
                                 reject(err);
                             })
-                    }).catch((err) => reject(err))
+                        }).catch((err) => reject(err))
                 }).catch((err) => reject(err))
             }).catch((err) => reject(err))
         }).catch((err) => reject(err))
