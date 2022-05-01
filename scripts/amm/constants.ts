ac37cc7f773d0228fa8feee48ce8e720c330f22e
Wayne Kuo <wayne.k@alphabit.software>
2022-05-01 14:04:31 +0800

    Fix contractSize

--- scripts/amm/constants.ts
+++ scripts/amm/constants.ts
@@ -1,7 +1,2 @@
-import { PublicKey } from "@solana/web3.js";
-
 // the index of amm to be used in other scipts
 export const ammIndex = 1;
-
-// the address of amm to be used in other scipts
-export const ammAddress = new PublicKey("Bkrvdu8r5r4zpcBFLWKF1ViWAhMyqtMRqbiSeQ2WBUdr")
