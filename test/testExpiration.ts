ec09c67e5383fc73fad2dcf5b713747591a4baaf
Prince <prince.gao@aloha.software>
2022-05-03 23:06:56 +0800

    updated test expiration

--- test/testExpiration.ts
+++ test/testExpiration.ts
@@ -1,12 +1,13 @@
 import * as assert from "assert";
-import {calculateSerumMarketsCount} from "../constants";
-import {generateExpirations} from "../utils/chains";
+import { calculateSerumMarketsCount } from "../constants";
+import { generateExpirations } from "../utils/chains";
 
-describe('Expirations', () => {
-    it('Properly calculates expirations', () => {
-        let markets = calculateSerumMarketsCount();
-        console.log("Serum markets are", markets);
-        let expirations = generateExpirations();
-        console.log("Expirations are ", expirations);
-    })
-})
+function test() {
+    let markets = calculateSerumMarketsCount();
+    console.log("Serum markets are", markets);
+    let expirations = generateExpirations();
+    console.log("Expirations are ", expirations);
+
+}
+
+test()
