49a5d4709cb5e6fd9b3dc7f445193cf58e84df32
Prince <prince.gao@aloha.software>
2022-05-03 09:48:59 +0800

    updated idl

--- constants.ts
+++ constants.ts
@@ -113,7 +113,7 @@ export const USDC_TOKEN_MINT: EndpointConstant = {
 }
 
 export const OPTIFI_EXCHANGE_ID: EndpointConstant = {
-    "https://api.devnet.solana.com": "211223",
+    "https://api.devnet.solana.com": "111300",
     "https://api.testnet.solana.com": "dmeWlh",
     "https://api.mainnet-beta.solana.com": "dmeWlh",
 }
