3aacd7b5fbccca30f20e9a59b4031e0e7d03326f
Prince Gao <prince.gao@aloha.software>
2022-05-16 16:12:13 +0800

    added update exchange authority

---
+++ instructions/authority/authUpdateExchangeAuthority.ts
@@ -0,0 +1,27 @@
+import Context from "../../types/context";
+import InstructionResult from "../../types/instructionResult";
+import { PublicKey, TransactionSignature } from "@solana/web3.js";
+import { findExchangeAccount } from "../../utils/accounts";
+
+export default function updateExchangeAuthority(context: Context,
+    newAuthority: PublicKey,
+): Promise<InstructionResult<TransactionSignature>> {
+    return new Promise((resolve, reject) => {
+        findExchangeAccount(context).then(([exchangeAddress, _]) => {
+            context.program.rpc.updateExchangeAuthority(
+                newAuthority,
+                {
+                    accounts: {
+                        optifiExchange: exchangeAddress,
+                        authority: context.provider.wallet.publicKey
+                    }
+                }
+            ).then((res) => {
+                resolve({
+                    successful: true,
+                    data: res as TransactionSignature
+                })
+            }).catch((err) => reject(err))
+        }).catch((err) => reject(err))
+    })
+}
