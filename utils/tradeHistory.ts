86b5104edbd98114bf9b740487527277d747d05d
ChunWang1998 <jjooee1998@gmail.com>
2022-05-24 10:43:04 +0800

    tradeHistory finish

--- utils/tradeHistory.ts
+++ utils/tradeHistory.ts
@@ -1,24 +1,58 @@
 import { OptifiMarket } from "../types/optifi-exchange-types";
 import { PublicKey, Keypair, Connection } from "@solana/web3.js";
-import { findAMMAccounts } from "./amm";
 import Context from "../types/context";
-import { createAccountRentExempt } from "@project-serum/common";
-import { Market, decodeInstruction } from "@project-serum/serum";
-import { SERUM_DEX_PROGRAM_ID, SOL_DECIMALS, USDC_DECIMALS } from "../constants";
-import bs58 from "bs58";
-import { BN } from "@project-serum/anchor";
-import { getAllOrdersForAccount } from "./orderHistory";
+
+import { initializeContext } from "../index";
+import { findUserAccount } from "../utils/accounts";
+import { loadOrdersAccountsForOwnerV2, loadOrdersForOwnerOnAllMarkets, Order } from "../utils/orders";
+import { findOptifiMarketsWithFullData } from "../utils/market";
+import { getAllOrdersForAccount } from "../utils/orderHistory";
+
+async function getOrders(context: Context): Promise<Order[]> {
+  return new Promise(async (resolve, reject) => {
+    let [userAccount,] = await findUserAccount(context)
+    let optifiMarkets = await findOptifiMarketsWithFullData(context)
+    let [userAccountAddress,] = await findUserAccount(context)
+
+    let openOrdersAccount = await loadOrdersAccountsForOwnerV2(context, optifiMarkets, userAccountAddress)
+    let context2 = await initializeContext(undefined, undefined, undefined, undefined, { disableRetryOnRateLimit: true, commitment: "confirmed" })
+    let orderHistory = await getAllOrdersForAccount(context2, userAccount,)
+    let orders = await loadOrdersForOwnerOnAllMarkets(optifiMarkets, openOrdersAccount.map(e => e.openOrdersAccount), orderHistory)
+    resolve(orders)
+  })
+}
+
+async function getPercentageFillPercentage(context: Context): Promise<number[]> {
+  return new Promise(async (resolve, reject) => {
+    let orders = await getOrders(context);
+
+    let res: number[] = [];
+
+    for (let i = 0; i < orders.length; i++) {
+      //@ts-ignore
+      res[orders[i].clientId.toNumber()] = orders[i].fillPercentage;
+    }
+    resolve(res);
+  })
+}
 
 export function getAllTradesForAccount(
   context: Context,
   account: PublicKey
 ): Promise<Trade[]> {
   return new Promise((resolve, reject) => {
-    getAllOrdersForAccount(context, account).then(res => {
+    getAllOrdersForAccount(context, account).then(async (res) => {
       // resolve(res)
       res.reverse()
       let trades: Trade[] = []
+
+      let ClientIdFilledPercentage: number[] = await getPercentageFillPercentage(context);
+      console.log(res)
       res.forEach(order => {
+// divide to three situations: place order / cancel order / fill
+// push to res if place order, pop res if cancel order
+// after that, check if fill order by ClientIdFilledPercentage, then renew res by it
+
         if (order.txType == "place order") {
           trades.push(new Trade({
             clientId: order.clientId,
@@ -36,25 +70,23 @@ export function getAllTradesForAccount(
           }))
         } else if (order.txType == "cancel order") {
           let index = trades.findIndex(e => e.clientId == order.clientId)
-          if (trades[index].side == "buy") {
-            if (trades[index].maxQuoteQuantity <= order.cancelledQuantity!) {
-              // remove the order if it's totally cancelled
-              trades.splice(index, 1)
-            } else {
-              // calc how much was filled
-              trades[index].maxQuoteQuantity -= order.cancelledQuantity!
-            }
-          } else {
-            if (trades[index].maxBaseQuantity <= order.cancelledQuantity!) {
-              // remove the order if it's totally cancelled
-              trades.splice(index, 1)
-            } else {
-              // calc how much was filled
-              trades[index].maxBaseQuantity -= order.cancelledQuantity!
-            }
-          }
+          trades.splice(index, 1)
         }
       })
+
+      for (let clientId = 0; clientId < ClientIdFilledPercentage.length; clientId++) {
+        if (ClientIdFilledPercentage[clientId] != null) {
+          if (ClientIdFilledPercentage[clientId] == 0) {//totally be filled, so delete from res
+            let index = trades.findIndex(e => e.clientId == clientId)
+            trades.splice(index, 1)
+          } else {// fill potential, so renew certain trade
+            let trade = trades.find(e => e.clientId == clientId)
+            //@ts-ignore
+            trade?.maxBaseQuantity = trade?.maxBaseQuantity * ClientIdFilledPercentage[clientId];
+          }
+        }
+      }
+
       trades.reverse()
       resolve(trades)
     }).catch(err => reject(err))
