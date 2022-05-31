5df0d2c5081c46680b0ecb103b193fceec06872c
Prince <prince.gao@aloha.software>
2022-05-31 15:03:09 +0800

    updated og nft scripts

--- scripts/ogNFT/airDropNft.ts
+++ scripts/ogNFT/airDropNft.ts
@@ -5,22 +5,57 @@ import fs from "fs"
 import { OG_NFT_MINT } from "../../constants";
 import { airdropNft } from "../../utils/ogNft";
 
+import * as XLSX from 'xlsx'
+
+
+/* load 'stream' for stream support */
+import { Readable } from 'stream';
+XLSX.stream.set_readable(Readable);
+
+// /* load the codepage support library for extended support with older formats  */
+// import * as cpexcel from 'xlsx';
+// XLSX.set_cptable(cpexcel);
+
 initializeContext().then(async (context) => {
     let nftMint = new PublicKey(OG_NFT_MINT[context.endpoint])
     let airdropAmount = 1
 
-    let filePath = path.resolve(__dirname, "addresses.json");
+    // let filePath = path.resolve(__dirname, "addresses.json");
+    // let addresses = JSON.parse(
+    //     fs.readFileSync(
+    //         filePath,
+    //         "utf-8"
+    //     )
+    // )
+    // for (let e of addresses) {
+    //     let name = Object.keys(e)[0]
+    //     let toWallet = new PublicKey(e[name])
+    //     let res = await airdropNft(context, nftMint, toWallet, airdropAmount)
+    //     console.log(`Sucessfully airdropped ${airdropAmount} OG NFT to ${name}'s wallet ${toWallet}, txid: ${res} `)
+    // }
+
+
+    // var workbook = XLSX.readFile("/Users/princegao/Desktop/optifi-sdk/scripts/ogNFT/OptiFi_OG_NFT_20220527.xlsx");
+
+    // let ogList = XLSX.utils.sheet_to_json(workbook.Sheets["1st batch"])
+    let filePath = path.resolve(__dirname, "addresses-20220527.json");
     let addresses = JSON.parse(
         fs.readFileSync(
             filePath,
             "utf-8"
         )
     )
+    console.log("addresses.length: ", addresses.length)
     for (let e of addresses) {
-        let name = Object.keys(e)[0]
-        let toWallet = new PublicKey(e[name])
-        let res = await airdropNft(context, nftMint, toWallet, airdropAmount)
-        console.log(`Sucessfully airdropped ${airdropAmount} OG NFT to ${name}'s wallet ${toWallet}, txid: ${res} `)
+        try {
+            let name = e["Discord ID"]
+            let toWallet = new PublicKey(e["Solana Address"].trim())
+                let res = await airdropNft(context, nftMint, toWallet, airdropAmount)
+                console.log(`Sucessfully airdropped ${airdropAmount} OG NFT to ${name}'s wallet ${toWallet}, txid: ${res} `)
+        } catch (err) {
+            console.log(e)
+            console.log(err)
+        }
     }
 })
 
--- utils/ogNft.ts
+++ utils/ogNft.ts
@@ -3,6 +3,7 @@ import { createAssociatedTokenAccountInstruction, createTransferInstruction, get
 import Context from "../types/context";
 import { OG_NFT_MINT } from "../constants";
 import { getTokenAccountFromAccountInfo } from "./token";
+import { BN } from "@project-serum/anchor";
 
 // check if a wallet address holds an og nft
 export async function checkIfUserHasOgNft(context: Context, userWallet?: PublicKey): Promise<boolean> {
@@ -34,24 +35,39 @@ export async function airdropNft(context: Context, nftMint: PublicKey, toWallet:
 
     let acctInfo = await context.connection.getAccountInfo(toAta)
     if (acctInfo == null) {
-        console.log(`Associated Token Account at ${toAta.toString()} did not exist, trying to create for user`);
+        console.log(`Associated Token Account at ${toAta.toString()} did not exist, trying to create for user ${toWallet}`);
         tx.add(createAssociatedTokenAccountInstruction(
             fromWallet.publicKey,
             toAta,
             toWallet,
             nftMint,
         ))
+        tx.add(
+            createTransferInstruction(
+                fromAta,
+                toAta,
+                fromWallet.publicKey,
+                airdropAmount,
+            )
+        );
+        let txid = await context.provider.send(tx)
+        return txid;
+    } else {
+        let tokenAccount = await getTokenAccountFromAccountInfo(acctInfo!, toAta)
+        if (new BN(tokenAccount.amount.toString()).toNumber() == 0) {
+            tx.add(
+                createTransferInstruction(
+                    fromAta,
+                    toAta,
+                    fromWallet.publicKey,
+                    airdropAmount,
+                )
+            );
+
+            let txid = await context.provider.send(tx)
+            return txid;
+
+        } else {
+        }
     }
-
-    tx.add(
-        createTransferInstruction(
-            fromAta,
-            toAta,
-            fromWallet.publicKey,
-            airdropAmount,
-        )
-    );
-
-    let txid = await context.provider.send(tx)
-    return txid;
 }
