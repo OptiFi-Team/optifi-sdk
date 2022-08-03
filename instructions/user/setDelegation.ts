b4e8f8af524ff6012cc14d6f066cad7b8c9fb165
Wayne Kuo <wayne.k@alphabit.software>
2022-08-03 15:25:36 -0600

    Create setDelegation.ts

---
+++ instructions/user/setDelegation.ts
@@ -0,0 +1,28 @@
+import Context from "../../types/context";
+import { PublicKey, TransactionSignature } from "@solana/web3.js";
+import InstructionResult from "../../types/instructionResult";
+import { findExchangeAccount, findUserAccount } from "../../utils/accounts";
+
+
+export default function setDelegation(context: Context, delegatee: String): Promise<InstructionResult<TransactionSignature>> {
+    return new Promise((resolve, reject) => {
+        findExchangeAccount(context).then(([exchangeAddress, _]) => {
+            findUserAccount(context).then(([userAccountAddress, _]) => {
+                context.program.rpc.setDelegation(
+                    new PublicKey(delegatee),
+                    {
+                        accounts: {
+                            optifiExchange: exchangeAddress,
+                            userAccount: userAccountAddress,
+                            user: context.provider.wallet.publicKey,
+                        }
+                    }).then((res) => {
+                        resolve({
+                            successful: true,
+                            data: res as TransactionSignature
+                        })
+                    }).catch((err) => reject(err))
+            }).catch((err) => reject(err))
+        }).catch((err) => reject(err))
+    })
+}
