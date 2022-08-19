c75e12a13b3aadd435096714de86209a61b7f99b
Wayne Kuo <wayne.k@alphabit.software>
2022-08-18 22:50:45 -0600

    Update 1.3.84

--- scripts/authority/authUpdateOracle.ts
+++ scripts/authority/authUpdateOracle.ts
@@ -2,7 +2,7 @@ import { initializeContext } from "../../index";
 import updateOracle from "../../instructions/authority/authUpdateOracle";
 import { Asset as OptifiAsset } from "../../types/optifi-exchange-types";
 
-const assets = [OptifiAsset.Bitcoin, OptifiAsset.Ethereum, OptifiAsset.USDC];
+const assets = [OptifiAsset.Bitcoin, OptifiAsset.Ethereum, OptifiAsset.USDC, OptifiAsset.Solana];
 
 initializeContext().then(async (context) => {
     for (let asset of assets) {
