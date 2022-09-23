3126f7fe24f107346909529fa56f61363bc85b82
Wayne Kuo <wayne.k@alphabit.software>
2022-09-23 17:22:48 +0800

    Fix no iv when expired

--- instructions/marginStress/updateIv.ts
+++ instructions/marginStress/updateIv.ts
@@ -24,46 +24,53 @@ export default function updateIv(context: Context):
 
                 let termStructure = await getGvolTermStructure(asset);
 
+                let iv;
+                let now;
+
                 termStructure.forEach(async element => {
                     if (element.expiration == marginStressAccount.expiryDate[0] * 1000) {
-                        let iv = element.markIv
-                        let now = Math.floor(Date.now() / 1000);
-
-                        if (asset == SUPPORTED_ASSETS[SUPPORTED_ASSETS.length - 1]) {
-                            let res = await context.program.rpc.updateIv(
-                                iv,
-                                new anchor.BN(now),
-                                {
-                                    accounts: {
-                                        optifiExchange: exchangeAddress,
-                                        marginStressAccount: marginStressAddress,
-                                        signer: context.provider.wallet.publicKey,
-                                    },
-                                    instructions: instructions
-                                }
-                            );
-
-                            resolve({
-                                successful: true,
-                                data: res as TransactionSignature
-                            })
-                        }
-                        else {
-                            let ix = context.program.instruction.updateIv(
-                                iv,
-                                new anchor.BN(now),
-                                {
-                                    accounts: {
-                                        optifiExchange: exchangeAddress,
-                                        marginStressAccount: marginStressAddress,
-                                        signer: context.provider.wallet.publicKey,
-                                    },
-                                }
-                            );
-                            instructions.push(ix);
-                        }
+                        iv = element.markIv
+                        now = Math.floor(Date.now() / 1000);
+                    } else {
+                        iv = await getGvolAtm7(asset);
+                        now = iv.date / 1000;
                     }
                 });
+
+
+                if (asset == SUPPORTED_ASSETS[SUPPORTED_ASSETS.length - 1]) {
+                    let res = await context.program.rpc.updateIv(
+                        iv,
+                        new anchor.BN(now),
+                        {
+                            accounts: {
+                                optifiExchange: exchangeAddress,
+                                marginStressAccount: marginStressAddress,
+                                signer: context.provider.wallet.publicKey,
+                            },
+                            instructions: instructions
+                        }
+                    );
+
+                    resolve({
+                        successful: true,
+                        data: res as TransactionSignature
+                    })
+                }
+                else {
+                    let ix = context.program.instruction.updateIv(
+                        iv,
+                        new anchor.BN(now),
+                        {
+                            accounts: {
+                                optifiExchange: exchangeAddress,
+                                marginStressAccount: marginStressAddress,
+                                signer: context.provider.wallet.publicKey,
+                            },
+                        }
+                    );
+                    instructions.push(ix);
+                }
             }
         } catch (e) {
             console.error(e);
