1ca8061c0823adb73e662c8cf17832c9fde4b1f6
Wayne Kuo <wayne.k@alphabit.software>
2022-09-29 16:30:57 +0800

    Fix liquidationToAmm

--- instructions/liquidation/liquidationToAmm.ts
+++ instructions/liquidation/liquidationToAmm.ts
@@ -1,5 +1,5 @@
 import Context from "../../types/context";
-import { PublicKey, TransactionSignature } from "@solana/web3.js";
+import { PublicKey, Transaction, TransactionInstruction, TransactionSignature } from "@solana/web3.js";
 import { findExchangeAccount, findLiquidationState, getDexOpenOrders } from "../../utils/accounts";
 import { OptifiMarket } from "../../types/optifi-exchange-types";
 import { findAssociatedTokenAccount, findOrCreateAssociatedTokenAccount } from "../../utils/token";
@@ -10,89 +10,103 @@ import { findMarginStressWithAsset } from "../../utils/margin";
 import { findOptifiMarketMintAuthPDA, getAmmLiquidityAuthPDA } from "../../utils/pda";
 import { findAMMAccounts } from "../../utils/amm";
 import { increaseComputeUnitsIx } from "../../utils/transactions";
+import marginStress from "../marginStress/marginStress";
 
-export default function liquidationToAmm(context: Context,
-    userAccountAddress: PublicKey,
-    marketAddress: PublicKey): Promise<InstructionResult<TransactionSignature>> {
+export default function liquidationToAmm(context: Context, userAccountAddress: PublicKey, marketAddress: PublicKey): Promise<InstructionResult<TransactionSignature>> {
     return new Promise((resolve, reject) => {
-        findExchangeAccount(context).then(([exchangeAddress, _]) => {
-            context.program.account.optifiMarket.fetch(marketAddress).then((marketRes) => {
-                let market = marketRes as OptifiMarket;
-                findLiquidationState(context, userAccountAddress).then(([liquidationStateAddress, _]) => {
-                    getSerumMarket(context, market.serumMarket).then((serumMarket) => {
-                        context.program.account.userAccount.fetch(userAccountAddress).then((userAccount) => {
-                            findOrCreateAssociatedTokenAccount(
-                                context,
-                                market.instrumentLongSplToken,
-                                userAccountAddress
-                            ).then((longSPLTokenVault) => {
-                                findOrCreateAssociatedTokenAccount(
-                                    context,
-                                    market.instrumentShortSplToken,
-                                    userAccountAddress
-                                ).then(async (shortSPLTokenVault) => {
+        findExchangeAccount(context)
+            .then(([exchangeAddress, _]) => {
+                context.program.account.optifiMarket
+                    .fetch(marketAddress)
+                    .then((marketRes) => {
+                        let market = marketRes as OptifiMarket;
+                        findLiquidationState(context, userAccountAddress)
+                            .then(([liquidationStateAddress, _]) => {
+                                getSerumMarket(context, market.serumMarket)
+                                    .then((serumMarket) => {
+                                        context.program.account.userAccount
+                                            .fetch(userAccountAddress)
+                                            .then((userAccount) => {
+                                                findOrCreateAssociatedTokenAccount(context, market.instrumentLongSplToken, userAccountAddress)
+                                                    .then((longSPLTokenVault) => {
+                                                        findOrCreateAssociatedTokenAccount(context, market.instrumentShortSplToken, userAccountAddress)
+                                                            .then(async (shortSPLTokenVault) => {
+                                                                let chainRes = await context.program.account.chain.fetch(marketRes.instrument);
+                                                                // @ts-ignore
+                                                                let chain = chainRes as Chain;
 
-                                    let chainRes = await context.program.account.chain
-                                        .fetch(marketRes.instrument)
-                                    // @ts-ignore
-                                    let chain = chainRes as Chain;
+                                                                let [marginStressAddress, _bump] = await findMarginStressWithAsset(context, exchangeAddress, chain.asset);
 
-                                    let [marginStressAddress, _bump] = await findMarginStressWithAsset(context, exchangeAddress, chain.asset);
+                                                                let [mintAuthAddress] = await findOptifiMarketMintAuthPDA(context);
 
-                                    let [mintAuthAddress] = await findOptifiMarketMintAuthPDA(context);
+                                                                let [ammLiquidityAuth] = await getAmmLiquidityAuthPDA(context);
 
-                                    let [ammLiquidityAuth] = await getAmmLiquidityAuthPDA(context)
+                                                                let [ammLongTokenVault] = await findAssociatedTokenAccount(
+                                                                    context,
+                                                                    market.instrumentLongSplToken,
+                                                                    ammLiquidityAuth
+                                                                );
 
-                                    let [ammLongTokenVault] = await findAssociatedTokenAccount(context, market.instrumentLongSplToken, ammLiquidityAuth);
+                                                                let [ammShortTokenVault] = await findAssociatedTokenAccount(
+                                                                    context,
+                                                                    market.instrumentShortSplToken,
+                                                                    ammLiquidityAuth
+                                                                );
 
-                                    let [ammShortTokenVault] = await findAssociatedTokenAccount(context, market.instrumentShortSplToken, ammLiquidityAuth);
+                                                                console.log("liquidationToAmm...");
+                                                                let ammAccounts = await findAMMAccounts(context);
+                                                                let ammForLiquidation = ammAccounts.filter((amm) => {
+                                                                    return amm[0].tradingInstruments.map((e) => e.toString()).includes(market.instrument.toString());
+                                                                });
 
-                                    console.log("liquidationToAmm...");
-                                    let ammAccounts = await findAMMAccounts(context)
-                                    let ammForLiquidation = ammAccounts.filter(amm => {
-                                       return amm[0].tradingInstruments.map(e => e.toString()).includes(market.instrument.toString())
+                                                                let instructions: TransactionInstruction[] = [increaseComputeUnitsIx];
+                                                                let updateMarginStressInx = await marginStress(context, ammForLiquidation[0][0].asset);
+                                                                instructions.push(...updateMarginStressInx);
+
+                                                                context.program.rpc
+                                                                    .liquidationToAmm({
+                                                                        accounts: {
+                                                                            optifiExchange: exchangeAddress,
+                                                                            marginStressAccount: marginStressAddress,
+                                                                            amm: ammForLiquidation[0][1],
+                                                                            userAccount: userAccountAddress,
+                                                                            userMarginAccount: userAccount.userMarginAccountUsdc,
+                                                                            liquidationState: liquidationStateAddress,
+                                                                            userInstrumentLongTokenVault: longSPLTokenVault,
+                                                                            userInstrumentShortTokenVault: shortSPLTokenVault,
+                                                                            instrumentLongSplTokenMint: serumMarket.decoded.baseMint,
+                                                                            instrumentShortSplTokenMint: market.instrumentShortSplToken,
+                                                                            instrumentTokenMintAuthorityPda: mintAuthAddress,
+                                                                            optifiMarket: marketAddress,
+                                                                            pcVault: serumMarket.decoded.quoteVault,
+                                                                            tokenProgram: TOKEN_PROGRAM_ID,
+                                                                            liquidator: context.provider.wallet.publicKey,
+                                                                            ammInstrumentLongTokenVault: ammLongTokenVault,
+                                                                            ammInstrumentShortTokenVault: ammShortTokenVault,
+                                                                            ammLiquidityAuth: ammLiquidityAuth,
+                                                                        },
+                                                                        preInstructions: instructions,
+                                                                    })
+                                                                    .then((res) => {
+                                                                        resolve({
+                                                                            successful: true,
+                                                                            data: res as TransactionSignature,
+                                                                        });
+                                                                    })
+                                                                    .catch((err) => reject(err));
+                                                            })
+                                                            .catch((err) => reject(err));
+                                                    })
+                                                    .catch((err) => reject(err));
+                                            })
+                                            .catch((err) => reject(err));
                                     })
-                                    context.program.rpc.liquidationToAmm(
-                                        {
-                                            accounts: {
-                                                optifiExchange: exchangeAddress,
-                                                marginStressAccount: marginStressAddress,
-                                                amm: ammForLiquidation[0][1],
-                                                userAccount: userAccountAddress,
-                                                userMarginAccount: userAccount.userMarginAccountUsdc,
-                                                liquidationState: liquidationStateAddress,
-                                                userInstrumentLongTokenVault:
-                                                    longSPLTokenVault,
-                                                userInstrumentShortTokenVault:
-                                                    shortSPLTokenVault,
-                                                instrumentLongSplTokenMint:
-                                                    serumMarket.decoded
-                                                        .baseMint,
-                                                instrumentShortSplTokenMint:
-                                                    market.instrumentShortSplToken,
-                                                instrumentTokenMintAuthorityPda: mintAuthAddress,
-                                                optifiMarket: marketAddress,
-                                                pcVault: serumMarket.decoded.quoteVault,
-                                                tokenProgram: TOKEN_PROGRAM_ID,
-                                                liquidator: context.provider.wallet.publicKey,
-                                                ammInstrumentLongTokenVault: ammLongTokenVault,
-                                                ammInstrumentShortTokenVault: ammShortTokenVault,
-                                                ammLiquidityAuth: ammLiquidityAuth,
-                                            },
-                                            preInstructions: [increaseComputeUnitsIx]
-                                        }
-                                    ).then((res) => {
-                                        resolve({
-                                            successful: true,
-                                            data: res as TransactionSignature
-                                        })
-                                    }).catch((err) => reject(err))
-                                }).catch((err) => reject(err))
-                            }).catch((err) => reject(err))
-                        }).catch((err) => reject(err))
-                    }).catch((err) => reject(err))
-                }).catch((err) => reject(err))
-            }).catch((err) => reject(err))
-        }).catch((err) => reject(err))
-    })
+                                    .catch((err) => reject(err));
+                            })
+                            .catch((err) => reject(err));
+                    })
+                    .catch((err) => reject(err));
+            })
+            .catch((err) => reject(err));
+    });
 }
