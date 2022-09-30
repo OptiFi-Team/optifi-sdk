cc36b9d82836c4b9edfc8cdd323207449d83804c
ChunWang1998 <jjooee1998@gmail.com>
2022-09-30 14:23:18 +0800

    getTradePrice func

---
+++ utils/getTradePrice.ts
@@ -0,0 +1,19 @@
+// input example
+// optifiProgramId: opDV2tLVsRPGk9aYqm4gdtGotiRwjuYKUmWzWB7NfCR
+// clientOrderId: 11
+// userAccountAddress: AAMWtCC2ieJ8rk2R9kAHJJwmsPNPctbH4W2P3pYZLDNF
+export async function getTradePrice(data: any) {
+    try {
+        let url = "https://lambda.optifi.app/get_trade_price?optifi_program_id=" + data.optifiProgramId +
+            "&client_order_id=" + data.clientOrderId +
+            "&user_account_address=" + data.userAccountAddress
+        const response = await fetch(url, {
+            method: "GET",
+        });
+        let res = await response.json()
+        if (!response.ok) {
+            console.log("no trade price");
+        }
+        return res;
+    } catch (error) { console.log(error) }
+}
