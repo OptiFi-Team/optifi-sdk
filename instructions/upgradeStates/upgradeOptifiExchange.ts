dafb6a24764faf0f9211cd410c89c590f211d3dc
Prince Gao <prince.gao@aloha.software>
2022-10-27 22:24:17 -0700

    1)updated idl; 2)updated exchange upgrade scripts

--- instructions/upgradeStates/upgradeOptifiExchange.ts
+++ instructions/upgradeStates/upgradeOptifiExchange.ts
@@ -4,10 +4,31 @@ import { TransactionSignature } from "@solana/web3.js";
 import { findExchangeAccount } from "../../utils/accounts";
 import { increaseComputeUnitsIx } from "../../utils/transactions";
 
-export default function upgradeOptifiExchange(context: Context): Promise<InstructionResult<TransactionSignature>> {
+export function upgradeOptifiExchangeV1ToV2(context: Context): Promise<InstructionResult<TransactionSignature>> {
     return new Promise((resolve, reject) => {
         findExchangeAccount(context).then(([exchangeAddress, _]) => {
-            context.program.rpc.upgradeOptifiExchange(
+            context.program.rpc.upgradeOptifiExchangeV1ToV2(
+                {
+                    accounts: {
+                        optifiExchange: exchangeAddress,
+                        exchangeAuthority: context.provider.wallet.publicKey
+                    },
+                    preInstructions: [increaseComputeUnitsIx]
+                }
+            ).then((res) => {
+                resolve({
+                    successful: true,
+                    data: res as TransactionSignature
+                })
+            }).catch((err) => reject(err))
+        }).catch((err) => reject(err))
+    })
+}
+
+export function upgradeOptifiExchangeV2ToV3(context: Context): Promise<InstructionResult<TransactionSignature>> {
+    return new Promise((resolve, reject) => {
+        findExchangeAccount(context).then(([exchangeAddress, _]) => {
+            context.program.rpc.upgradeOptifiExchangeV2ToV3(
                 {
                     accounts: {
                         optifiExchange: exchangeAddress,
--- scripts/upgradeStates/upgradeOptifiExchange.ts
+++ scripts/upgradeStates/upgradeOptifiExchange.ts
@@ -1,9 +1,9 @@
 // @ts-ignore
 import { initializeContext } from "../../index";
 import Context from "../../types/context";
-import upgradeOptifiExchange from "../../instructions/upgradeStates/upgradeOptifiExchange";
+import { upgradeOptifiExchangeV1ToV2, upgradeOptifiExchangeV2ToV3, } from "../../instructions/upgradeStates/upgradeOptifiExchange";
 
 initializeContext().then(async (context: Context) => {
-    let res = await upgradeOptifiExchange(context)
+    let res = await upgradeOptifiExchangeV2ToV3(context)
     console.log("upgradeOptifiExchange res: ", res)
 })
